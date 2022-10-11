73a22fbe9cdcd4dd2deefffbe8d1c93c055f1ed9
arifbiswas <mxgangstar310@gmail.com>
2022-10-11 19:17:25 +0600

    All completed and last check

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 Site titile 
 ## Ture Programmer Quizzes
+
+This Site is provide unlimited programming knowledge,
+Go Our Site and Enjoy true facilities [True Programing Quizzers](),
+
+## Facilities 
+Have many Quizzes ,
+Increase your knowledge of programming sector.
+Provide regular updated Quiz (You can see our total Quizzes FlowChart)[FlowChart]().
+
+### How can i enjoy your site ?
+At first go our home page [True Programing Quizzers](),And scroll down you can see 4 Quizzes section .This every section are open for your and fully free , Click Start Quizzes and Start your quiz journey ,Now try to give CorrectAnswer of every quiz.And Enjoy your self.
+
+### Nav name Section
+# Home
+# ShowChart
+# Blogs
+# Contact
+
+
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,32 +20,32 @@ const Contact = () => {
                         </svg>
 
                         <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">
-                            Cecilia Chapman 711-2880 Nulla
-                            St. Mankato Mississippi 96522
+                            jatrabari ,Dhaka-1204 ,Bangladesh  
+                            St. Nobinor Abuhaji  
                         </span>
                     </p>
 
-                    <p class="flex items-start -mx-2">
+                    <p class="flex items-end -mx-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
 
-                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">(257) 563-7401</span>
+                        <span class="mx-2 text-gray-700 truncate  dark:text-gray-400">(+880) 01871063074</span>
                     </p>
 
-                    <p class="flex items-start -mx-2">
+                    <p class="flex items-end -mx-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
 
-                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">acb@example.com</span>
+                        <span class="mx-2 text-gray-700 truncate  dark:text-gray-400">mxgangstar310@gamil.com</span>
                     </p>
                 </div>
 
                 <div class="mt-6 w-80 md:mt-8">
                     <h3 class="text-gray-600 dark:text-gray-300 ">Follow us</h3>
 
-                    <div class="flex mt-4 -mx-1.5 ">
+                    <div class="flex justify-center items-center mt-4 -mx-1.5 ">
                         <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500" href="#">
                             <svg class="w-10 h-10 fill-current" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M18.6668 6.67334C18.0002 7.00001 17.3468 7.13268 16.6668 7.33334C15.9195 6.49001 14.8115 6.44334 13.7468 6.84201C12.6822 7.24068 11.9848 8.21534 12.0002 9.33334V10C9.83683 10.0553 7.91016 9.07001 6.66683 7.33334C6.66683 7.33334 3.87883 12.2887 9.3335 14.6667C8.0855 15.498 6.84083 16.0587 5.3335 16C7.53883 17.202 9.94216 17.6153 12.0228 17.0113C14.4095 16.318 16.3708 14.5293 17.1235 11.85C17.348 11.0351 17.4595 10.1932 17.4548 9.34801C17.4535 9.18201 18.4615 7.50001 18.6668 6.67268V6.67334Z" />
@@ -83,12 +83,12 @@ const Contact = () => {
                     <form class="mt-6">
                         <div class="flex-1">
                             <label class="block mb-2 text-sm text-gray-600 dark:text-gray-900">Full Name</label>
-                            <input type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-100 dark:text-gray-900 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <input type="text" placeholder="Your Name" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-100 dark:text-gray-900 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
 
                         <div class="flex-1 mt-6">
                             <label class="block mb-2 text-sm text-gray-600 dark:text-gray-900">Email address</label>
-                            <input type="email" placeholder="johndoe@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-100 dark:text-gray-900 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <input type="email" placeholder="Your@gmail.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-100 dark:text-gray-900 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
 
                         <div class="w-full mt-6">
